1140bd600164593775f2bb690e339d66a2946345
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2016-12-11 10:56:49 +0100

    Improve date handling (with JS date format)

--- app/MeetingDay.php
+++ app/MeetingDay.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -12,6 +13,11 @@ class MeetingDay extends Model
   protected $hidden = ['meeting_id', 'id'];
   protected $dates = ['day'];
   public $timestamps = false;
+
+  public function setDayAttribute($value)
+  {
+    $this->attributes['day'] = Carbon::createFromFormat('Y-m-d\\TH:i:s.uZ', $value);
+  }
 
   public function hours(): HasMany
   {
