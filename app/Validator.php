2f33b2ff63fbad9e8a4c71a4090ca7d550c2c2f0
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2017-03-02 22:03:58 +0100

    Improve hours validation for meeting creation

--- app/Validator.php
+++ app/Validator.php
@@ -18,27 +18,32 @@ class Validator extends \Illuminate\Validation\Validator
   {
     $this->requireParameterCount(2, $parameters, 'after_at_least');
 
-    $enhanceValue = function ($value)
-    {
-      if(strpos($value, ':') === false)
-      {
-        return $value.':00';
-      }
-
-      return $value;
-    };
-
     $afterValue = $this->getValue($parameters[0]);
-    $current = Carbon::createFromFormat('H:i', $enhanceValue($value));
-    $before = Carbon::createFromFormat('H:i', $enhanceValue($afterValue));
+    $current = $this->createTimeFromValue($value);
+    $before = $this->createTimeFromValue($afterValue);
     $difference = $this->getValue($parameters[1]);
     $current->subMinute($difference);
 
     return $before->lte($current);
   }
 
+  /**
+   * @param $value
+   * @return Carbon
+   */
+  private function createTimeFromValue($value)
+  {
+    if(strpos($value, ':') === false)
+    {
+      $value .= ':00';
+    }
+
+    return Carbon::createFromFormat('H:i', $value);
+  }
+
   public function validateSimpleHour($attribute, $value)
   {
-    return preg_match('@^\d{1,2}(:\d{2})?$@', $value) === 1;
+    return preg_match('@^\d{1,2}(:\d{2})?$@', $value) === 1 &&
+      $this->createTimeFromValue($value)->lte(Carbon::now()->endOfDay());
   }
 }
