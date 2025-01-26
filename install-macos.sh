1ba405449c4c89047bb60cf64a609a1ce810281c
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2025-01-26 17:56:46 +0100

    Add missing libpq for Postgres support

--- install-macos.sh
+++ install-macos.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# Install dependencies
+# Install PHP dependencies
 brew install bison re2c gmp libsodium imagemagick pkg-config libgd libiconv oniguruma libzip openssl
+# Install app dependencies
+brew install libpq
 # Install PHP
-PHP_CONFIGURE_OPTIONS="--with-openssl=$(brew --prefix openssl) --with-iconv=$(brew --prefix libiconv)" PHP_WITHOUT_PEAR=yes asdf install
+PHP_CONFIGURE_OPTIONS="--with-openssl=$(brew --prefix openssl) --with-iconv=$(brew --prefix libiconv)" asdf install
 
